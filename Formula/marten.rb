ed3dd84dabe50f65c99ebf16d98d8b03ead2b47b
Morgan Aubert <morgan@aubert.email>
2022-08-27 18:11:36 -0400

    Add new release

--- Formula/marten.rb
+++ Formula/marten.rb
@@ -3,9 +3,9 @@ require_relative "lib/private_strategy"
 class Marten < Formula
   desc "CLI client for initializing new Marten projects and applications"
   homepage "http://github.com/martenframework/marten"
-  url "https://raw.githubusercontent.com/martenframework/homebrew-marten/master/tarballs/marten-0.1.0.dev0.20220807192952.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
-  sha256 "688727fb5ed927715adb9e433f3a4ee1435faf1884705fff2a78cd3552f8c6ac"
-  version "0.1.0.dev0.20220807192952"
+  url "https://raw.githubusercontent.com/martenframework/homebrew-marten/master/tarballs/marten-0.1.0.dev0.20220827181126.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+  sha256 "3601e272b66bd07d194560fc02134ef3e24daf30efb13ea23ac195d79a27cd14"
+  version "0.1.0.dev0.20220827181126"
   depends_on "crystal"
 
   def install
