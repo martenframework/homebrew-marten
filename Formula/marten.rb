ab43209e2206b70f08d6c0b0866f41f8c4fa5ee7
Morgan Aubert <morgan@aubert.email>
2023-06-19 12:00:04 -0400

    Add 0.3.0 release

--- Formula/marten.rb
+++ Formula/marten.rb
@@ -1,9 +1,9 @@
 class Marten < Formula
   desc "CLI client for initializing new Marten projects and applications"
   homepage "http://github.com/martenframework/marten"
-  url "https://raw.githubusercontent.com/martenframework/homebrew-marten/master/tarballs/marten-0.2.4.tar.gz"
-  sha256 "6f57d0e0fd1bd85665dc4aa2e870aa46ab671f3682f6eafb51e477d47863778c"
-  version "0.2.4"
+  url "https://raw.githubusercontent.com/martenframework/homebrew-marten/master/tarballs/marten-0.3.0.tar.gz"
+  sha256 "fd6ceefa1381ee4f7fb0e2ae23545198779495d31f859c67b842db210c5a61a7"
+  version "0.3.0"
   depends_on "crystal"
 
   def install
